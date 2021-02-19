603419f183fac61f55eef7982a1640facf510e16
tktk2580 <8520tktk@gmail.com>
2021-02-19 13:23:23 +0900

    Extract components

---
+++ src/components/MemoList.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function MemoList() {
+  return (
+    <View>
+      <View style={styles.memoListItem}>
+        <View>
+          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+          <Text style={styles.memoListItmeDate}>2020年12月24日 10:00</Text>
+        </View>
+        <View>
+          <Text>X</Text>
+        </View>
+      </View>
+      <View style={styles.memoListItem}>
+        <View>
+          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+          <Text style={styles.memoListItmeDate}>2020年12月24日 10:00</Text>
+        </View>
+        <View>
+          <Text>X</Text>
+        </View>
+      </View>
+      <View style={styles.memoListItem}>
+        <View>
+          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+          <Text style={styles.memoListItmeDate}>2020年12月24日 10:00</Text>
+        </View>
+        <View>
+          <Text>X</Text>
+        </View>
+      </View>
+      <View style={styles.memoListItem}>
+        <View>
+          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+          <Text style={styles.memoListItmeDate}>2020年12月24日 10:00</Text>
+        </View>
+        <View>
+          <Text>X</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  memoListItem: {
+    backgroundColor: '#ffffff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 15,
+    paddingHorizontal: 19,
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0,0,0,0.15)',
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+  memoListItmeDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: '#848484',
+  },
+});
