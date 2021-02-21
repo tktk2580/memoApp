cfa624e98c2d3e6766b5b6e04c0b73a9f907f1a1
tktk2580 <8520tktk@gmail.com>
2021-02-21 09:31:36 +0900

    save on firebase

--- App.jsx
+++ App.jsx
@@ -12,11 +12,13 @@ import SignupScreen from './src/screens/SignupScreen';
 
 import { firebaseConfig } from './env';
 
-const Stack = createStackNavigator();
+require('firebase/firestore');
 
 if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig);
 }
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
